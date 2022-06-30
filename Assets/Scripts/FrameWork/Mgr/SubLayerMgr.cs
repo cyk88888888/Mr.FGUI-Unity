8cd8e3f0dc5965953a70e2ea884419d9ddb0e467
cyk <935765353@qq.com>
2022-07-01 00:09:54 +0800

    初版

--- Assets/Scripts/FrameWork/Mgr/SubLayerMgr.cs
+++ Assets/Scripts/FrameWork/Mgr/SubLayerMgr.cs
@@ -1,11 +1,121 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SubLayerMgr 
+public class SubLayerMgr
 {
+    private Dictionary<string, Type> _classMap;
+    public UILayer curLayer;
+    private List<UILayer> _popArr;
     public SubLayerMgr()
     {
+        _classMap = new Dictionary<string, Type>();
+        _popArr = new List<UILayer>();
+    }
 
+    /**
+    * 注册子页面
+    * @param layerClass 
+    */
+    public void register(string layerName, object opt)
+    {
+        _classMap[layerName] = Type.GetType(layerName);
+    }
+
+    /**显示指定界面（替换模式） */
+    public void run(string layerName, object data = null)
+    {
+        _show(layerName, data);
+    }
+
+    /**显示指定界面（入栈模式） */
+    public void push(string layerName, object data = null)
+    {
+        _show(layerName, data, true);
+    }
+
+    private void _show(string layerName, object data, bool toPush = false)
+    {
+        if (curLayer && curLayer.className == layerName) return;//打开同个界面
+
+        Type registerLayer = _classMap[layerName];
+        bool needDestory = registerLayer == null && !toPush;//未注册  && 非入栈模式
+
+        this.checkDestoryLastLayer(needDestory);
+
+        if (this.curLayer)
+        {
+            if (toPush) _popArr.Add(curLayer);
+            if (toPush || !needDestory)
+            {
+                this.curLayer.removeSelf();
+            }
+        }
+
+        if (registerLayer != null && registerLayer is UILayer)
+        {
+            this.curLayer = registerLayer;
+            this.curLayer.addSelf();
+            return;
+        }
+
+        this.curLayer = script.show(data);
+        if (_classMap[layerName] != null)
+        {
+            this._classMap[layerName] = curLayer;
+        }
+    }
+
+    /**判断销毁上个界面并释放资源 */
+    private void checkDestoryLastLayer(bool destory = false)
+    {
+        if (destory && this.curLayer && !this.curLayer.hasDestory)
+        {
+            this.curLayer.close();
+        }
+    }
+
+    /** layer出栈*/
+    public void pop()
+    {
+        if (_popArr.Count <= 0)
+        {
+            Debug.LogError("已经pop到底了！！！！！！！");
+            return;
+        }
+        checkDestoryLastLayer(true);
+        curLayer = _popArr[_popArr.Count];
+        _popArr.RemoveAt(_popArr.Count);
+        curLayer.addSelf();
+    }
+
+    /**清除所有注册的layer */
+    public void releaseAllLayer()
+    {
+        checkDestoryLastLayer(true);
+        for (let i = 0; i < self._popArr.length; i++)
+        {
+            let layer = this._popArr[i];
+            if (!layer.hasDestory) layer.close();
+        }
+
+        for (let key in this._classMap)
+        {
+            let layer = this._classMap[key];
+            if (layer.node && !layer.hasDestory)
+            {
+                layer.close();
+            }
+        }
+
+        _popArr = new List<UILayer>();
+    }
+
+    public void dispose()
+    {
+        releaseAllLayer();
+        _classMap = null;
+        _popArr = null;
     }
 }
--- Assets/Scripts/FrameWork/UI/UIComp.cs
+++ Assets/Scripts/FrameWork/UI/UIComp.cs
@@ -21,9 +21,10 @@ public class UIComp : MonoBehaviour
     /// 皮肤
     /// </summary>
     protected GComponent view;
+    private GComponent _oldParent;
     public object data;
     private bool isFirstEnter = true;
-
+    public bool hasDestory = false;
     public UIComp()
     {
         ctor_b();
@@ -58,12 +59,58 @@ public class UIComp : MonoBehaviour
         _dispose();
     }
 
+    private void OnDestroy()
+    {
+        Debug.LogError("onDestroy: " + gameObject.name);
+    }
+
+    /// <summary>
+    /// 设置view的父级
+    /// </summary>
+    /// <param name="parent"></param>
+    public void setParent(GComponent parent)
+    {
+        if (view == null) return;
+        _oldParent = parent;
+        parent.AddChild(view);
+    }
+
     private void __doEnter()
     {
         Debug.Log("进入" + gameObject.name);
+        initView();
     }
 
-    private void _dispose() {
+    private void initView()
+    {
+
+    }
+
+    public string className
+    {
+        get {
+            return name;
+        }
+    }
+    public void close()
+    {
+        //onCloseAnimation(() => {
+        //    self.destory();
+        //});
+    }
+
+    public void addSelf()
+    {
+        setParent(_oldParent);
+    }
+
+    public void removeSelf()
+    {
+        view.RemoveFromParent();
+    }
+
+    private void _dispose()
+    {
         //self.clearAllTimeoutOrInterval();
         //self.rmAllTweens();
 
@@ -71,5 +118,12 @@ public class UIComp : MonoBehaviour
         onExit_b();
         onExit();
         onExit_a();
+    }
+
+    public void destory()
+    {
+        if (hasDestory) return;
+        view.Dispose();
+        hasDestory = true;
     }
 }
