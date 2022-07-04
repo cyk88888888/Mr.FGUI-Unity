a1b997e6a1220cbdd4ca47aeb057e4375936ab62
cyk <935765353@qq.com>
2022-07-05 00:32:11 +0800

    人物模型资源

--- Assets/Scripts/Module/Home/HomeLayer.cs
+++ Assets/Scripts/Module/Home/HomeLayer.cs
@@ -24,6 +24,13 @@ public class HomeLayer : UILayer
         btn_bag.onClick.Add(() => {
             Show("BagDlg");
         });
+
+        Object prefab = Resources.Load("Role/npc");
+        GameObject go = (GameObject)Object.Instantiate(prefab);
+        go.transform.localPosition = new Vector3(61, -89, 1000); //set z to far from UICamera is important!
+        go.transform.localScale = new Vector3(180, 180, 180);
+        go.transform.localEulerAngles = new Vector3(0, 100, 0);
+        view.GetChild("holder").asGraph.SetNativeObject(new GoWrapper(go));
     }
 
 
