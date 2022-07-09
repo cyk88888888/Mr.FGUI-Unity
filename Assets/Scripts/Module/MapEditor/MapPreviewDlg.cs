7fbf5e64ab00b863930d70b00d685168fbedf008
cyk <935765353@qq.com>
2022-07-10 00:35:24 +0800

    地图预览优化

--- Assets/Scripts/Module/MapEditor/MapPreviewDlg.cs
+++ Assets/Scripts/Module/MapEditor/MapPreviewDlg.cs
@@ -30,33 +30,43 @@ public class MapPreviewDlg : UIDlg
         btn_close.onClick.Add(() => { Close(); });
         GComponent previewComp = view.GetChild("previewComp").asCom;
         holder = previewComp.GetChild("holder").asGraph;
-        
+
         //RenderTexture renderTexture = (RenderTexture)__data;
         Texture2D renderTexture = (Texture2D)__data;
         _renderTextureSize = new(renderTexture.width, renderTexture.height);
-        holder.SetSize(renderTexture.width, renderTexture.height);
+        curScale = GRoot.inst.height / renderTexture.height;
         _image = new Image();
         holder.SetNativeObject(_image);
         _image.texture = new NTexture(renderTexture);
         _image.blendMode = BlendMode.Off;
-        
+        holder.SetSize(_renderTextureSize.x * curScale, _renderTextureSize.y * curScale);
+        UpdateHoldXY();
         view.displayObject.onMouseWheel.Add(_onMouseWheel);
     }
 
+    private float curScale;
+    private float scaleDelta = 0.01f;
     private void _onMouseWheel(EventContext context)
     {
         InputEvent inputEvt = (InputEvent)context.data;
-        float scaleDelta = 0.05f;
         if (inputEvt.mouseWheelDelta > 0)
         {
-            holder.SetScale(holder.scaleX - scaleDelta, holder.scaleY - scaleDelta); 
+            if (Mathf.Floor(holder.width) <= GRoot.inst.width && Mathf.Floor(holder.height) <= GRoot.inst.height) return; ;//已全部可见
+            curScale -= scaleDelta;
         }
         else
         {
-            holder.SetScale(holder.scaleX + scaleDelta, holder.scaleY + scaleDelta);
+            if (Mathf.Floor(holder.width) >= _renderTextureSize.x && Mathf.Floor(holder.height) >= _renderTextureSize.y) return;//已达到原大小
+            curScale += scaleDelta;
         }
-        holder.SetSize(_renderTextureSize.x * holder.scaleX, _renderTextureSize.y * holder.scaleX);
-        Debug.Log("holder.width: " + holder.width + ", holder.height: " + holder.height);
+      
+        holder.SetSize(_renderTextureSize.x * curScale, _renderTextureSize.y * curScale);
+        UpdateHoldXY();
+        Debug.Log("holder.width: " + holder.width + ", holder.height: " + holder.height + ", curScale: " + curScale);
+    }
+
+    private void UpdateHoldXY()
+    {
         holder.x = holder.width < GRoot.inst.width ? (GRoot.inst.width - holder.width) / 2 : 0;
         holder.y = holder.height < GRoot.inst.height ? (GRoot.inst.height - holder.height) / 2 : 0;
     }
